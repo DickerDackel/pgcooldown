aafac8df59732e967ac90eb6d86c960f71408be3
Michael Lamertz <michael.lamertz@gmail.com>
2023-05-02 19:09:05 +0200

    *bump*

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='cooldown',
     description='A cooldown/counter class to trigger time based events in games',
-    version='0.1.6',
+    version='0.1.7',
     author='Michael Lamertz',
     author_email='michael.lamertz@gmail.com',
     url='https://github.com/dickerdackel/cooldown',
